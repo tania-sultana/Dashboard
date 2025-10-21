e88a85b0dec823e210eb0faf634f52f76933570c
tania <tania.190142@s.pust.ac.bd>
2025-10-21 13:36:38 +0600

    complete dashboard design

--- index.js
+++ index.js
@@ -42,3 +42,35 @@
         chart.render();
       
       
+
+
+        // --sidebar--
+        
+    document.addEventListener("DOMContentLoaded", function () {
+        const sideItems = document.querySelectorAll(".side");
+        sideItems.forEach(item => {
+            item.addEventListener("click", function () {              
+                sideItems.forEach(el => el.classList.remove("active"));          
+                this.classList.add("active");
+            });
+        });
+    });
+
+
+
+  // const toggleBtn = document.getElementById('sidebarToggle');
+  // const sidebar = document.getElementById('sidebar');
+
+  // toggleBtn.addEventListener('click', () => {
+  //   sidebar.classList.toggle('show');
+  // });
+
+  // document.addEventListener('click', function (event) {
+  //   const isClickInside = sidebar.contains(event.target) || toggleBtn.contains(event.target);
+
+  //   if (!isClickInside && sidebar.classList.contains('show')) {
+  //     sidebar.classList.remove('show');
+  //   }
+  // });
+
+
